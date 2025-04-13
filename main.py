89f92ecb693f5cd92b1f9c5a229cbb4fc99f648d
hssKang <150224614+hssKang@users.noreply.github.com>
2025-04-13 14:37:46 +0900

    feat: DB 연결
    JIRA: GUC-74
    resolves: #10

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-import os
-from dotenv import load_dotenv
-import threading
-import json
-import google.generativeai as genai
-from groq import Groq
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse, FileResponse
 from pydantic import BaseModel
 import httpx
 import tempfile
 
-
 from src.READMECreater.GithubFetcher import DownloadRepoFiles
 from src.READMECreater.READMEGenerator import GenerateREADME
-from src.TagCreater.READMEFetcher import GetREADME
-from src.TagCreater.Models import CallLLM, CallGemini, ExtractJson, results
-from src.TagCreater.TagMerger import MergeCleanTags
-from src.Upload.Upload2DB import GetVersion, SaveMetadata, GetNextReadmeID
+from src.TagCreater.Models import ExtractJson, ModelThreading
+from src.Upload.Upload2DB import (
+    GetVersion,
+    SaveGitData,
+    GetNextReadmeID,
+    SavingCareerDB,
+)
 from src.Upload.Upload2Storage import UploadREADME
 from src.Utils.GetJWT import GetDataFromToken, GetTokenFromHeader
+from src.Utils.GetImage import GetImageInGithub
+from src.Utils.DBClient import ReadGithubFromUserID, ReadREADMEDB
 
-app = FastAPI(title="GitHub README Generator API")
+app = FastAPI(title="GitHub AI API")
 
 
 class RepoRequest(BaseModel):
@@ -46,7 +44,7 @@ async def GenerateReadme(request: RepoRequest):
 
         # README Storage & DB에 저장
         downloadURL = UploadREADME(readmeContent, userID, repoURL, version)
-        SaveMetadata(version, repoURL, readmeID, userID, downloadURL)
+        SaveGitData(version, repoURL, readmeID, userID, downloadURL)
 
         return {
             "status": 200,
@@ -96,62 +94,62 @@ async def DownloadFile(downloadURL: str):
 @app.post("/api/career/tag")
 async def GenerateTag(request: RepoRequest):
     try:
-        # API Keys
-        envPath = os.path.join(os.path.dirname(__file__), "..", ".env")
-        load_dotenv(dotenv_path=os.path.abspath(envPath))
-        GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-        GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-        GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+        githubURL = request.git_url
+        response = ModelThreading(githubURL)
+        tags = ExtractJson(response.text)
 
-        # Input
-        url = request.git_url
-        readmeContent = GetREADME(url, GITHUB_TOKEN)
-
-        # 모델 부르기
-        client = Groq(api_key=GROQ_API_KEY)
-        genai.configure(api_key=GOOGLE_API_KEY)
-        gemini = genai.GenerativeModel("gemini-2.0-flash-thinking-exp-01-21")
-
-        # LLM Thread에서 호출
-        models = ["qwen-2.5-coder-32b", "llama-3.3-70b-versatile"]
-        threads = [
-            threading.Thread(target=CallLLM, args=(model, readmeContent, client))
-            for model in models
-        ]
-        for thread in threads:
-            thread.start()
-
-        geminiThread = threading.Thread(target=CallGemini, args=(readmeContent, gemini))
-        threads.append(geminiThread)
-        geminiThread.start()
-
-        for thread in threads:
-            thread.join()
-
-        # Output
-        for model, tags in results.items():
-            print(f"Model: {model}")
-            print(tags)
-            print("-" * 50)
-
-        FinalTags = MergeCleanTags(*[results[m] for m in results])
-        resultJson = {"tags": FinalTags}
-
-        refiner = genai.GenerativeModel("learnlm-1.5-pro-experimental")
-        response = refiner.generate_content(
-            f'{json.dumps(resultJson, ensure_ascii=False)} Extract only key technologies in JSON format as "tags": []'
-        )
+        """ 이 부분은 token 연동 후 """
+        # accessToken = GetTokenFromHeader(request)
+        # userID = GetDataFromToken(accessToken, "user_id")
+        userID = 312
+        imageURL = GetImageInGithub(githubURL)
+        SavingCareerDB(tags, userID, githubURL, imageURL)
 
         return {
             "status": 200,
             "message": "요청에 성공하였습니다.",
-            "data": ExtractJson(response.text),
+            "data": tags,
         }
 
     except Exception:
         return JSONResponse(
             status_code=400,
             content={"status": 400, "message": "Tag 생성 실패", "data": None},
+        )
+
+
+# 유저가 올린 모든 github 정보 읽기
+@app.get("/api/career/db/user")
+async def ReadUserGithub(userID: int):
+    try:
+        data = ReadGithubFromUserID(userID)
+
+        return {
+            "status": 200,
+            "message": "요청에 성공하였습니다.",
+            "data": data,
+        }
+    except Exception:
+        return JSONResponse(
+            status_code=400,
+            content={"status": 400, "message": "DB read 실패", "data": None},
+        )
+
+
+# 유저가 올린 README 정보 읽기
+@app.get("/api/career/db/readme")
+async def ReadREADME(userID: int, gitURL: str):
+    try:
+        data = ReadREADMEDB(userID, gitURL)
+        return {
+            "status": 200,
+            "message": "요청에 성공하였습니다.",
+            "data": data,
+        }
+    except Exception:
+        return JSONResponse(
+            status_code=400,
+            content={"status": 400, "message": "DB read 실패", "data": None},
         )
 
 
